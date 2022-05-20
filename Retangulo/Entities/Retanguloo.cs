15ae791e17c8bdeed8a3f06357e1338c3dd6bb1a
Rafael Kowalski <rafaelmk06@gmail.com>
2022-05-19 21:08:44 -0300

    Add project files.

---
+++ Retangulo/Entities/Retanguloo.cs
@@ -0,0 +1,26 @@
+﻿namespace Retangulo.Entities
+{
+    public class Retanguloo
+    {
+        public double Altura { get; set; }
+        public double Largura { get; set; }
+
+        public Retanguloo(double altura, double largura)
+        {
+            Altura = altura;
+            Largura = largura;
+        }
+
+        public double Area()
+        {
+            double area = Altura * Largura;
+            return area;
+        }
+
+        public double Perimetro()
+        {
+            double perimetro = Altura * 2 + Largura * 2;
+            return perimetro;
+        }
+    }
+}
---
+++ Retangulo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Retangulo.Entities;
+
+namespace Retangulo;
+
+    public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Informe a altura do retangulo:");
+        double altura = double.Parse(Console.ReadLine());
+        Console.Write("Informe a largura do retangulo:");
+        double largura = double.Parse(Console.ReadLine());
+
+        Retanguloo retangulo = new Retanguloo(altura, largura);
+        double area = retangulo.Area();
+        Console.WriteLine("Area = " + area.ToString("F2"));
+
+        double perimetro = retangulo.Perimetro();
+        Console.WriteLine("Perimetro = " + perimetro.ToString("F2"));
+    }
+
+}
